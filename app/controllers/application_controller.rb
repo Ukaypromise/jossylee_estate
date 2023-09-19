496c8bec7c0333cbd1f61022fccce3398def9a9e
Ukaypromise <promiseukaimo@gmail.com>
2023-09-19 23:00:15 +0100

    Setup devise and pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+  # after_action :verify_authorized, except: :index
+  # after_action :verify_policy_scoped, only: :index
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def user_not_authorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+    flash[:alert] = t "#{policy_name}.#{exception.query}", scope: "pundit", default: :default
+    redirect_to(request.referer || root_path)
+  end
+
+  protect_from_forgery with: :exception
+  before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:email, :password, :password_confirmation, :current_password)
+    end
+  end
+
+  def after_sign_in_path_for(_resource_or_scope)
+    root_path
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  enum role: {
+    client: "client",
+    marketer_partner: "marketer_partner",
+    manager: "manager",
+    director: "director",
+    ceo: "ceo",
+  }
+
+  after_initialize do
+    if self.new_record?
+      self.role ||= :client
+    end
+  end
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+end
