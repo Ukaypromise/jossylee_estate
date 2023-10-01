9fe529cff011cbe97e14fe687f5b0a9da6beed9b
Ukaypromise <promiseukaimo@gmail.com>
2023-10-01 13:52:49 +0100

    Create some seed files and modify property partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create 20 sample properties with images
+20.times do |i|
+  property = Property.create(
+    name: "Property #{i + 1}",
+    description: "Description for Property #{i + 1}",
+    property_type: "Residential",
+    location: "Location #{i + 1}",
+    price: 150000 + (i * 1000),  # Adjust the prices as needed
+    plot_size: 1200 + (i * 100), # Adjust the plot sizes as needed
+    property_status: "For Sale",
+    availability: Date.today + (i * 5), # Adjust availability dates as needed
+    owner_agent: "Real Estate Agency",
+    # user: user, # Associate the property with the user
+  )
+end
