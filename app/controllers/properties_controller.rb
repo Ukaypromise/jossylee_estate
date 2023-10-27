b2e62230a38492ed2ec9e78d6800b2089df04e00
Ukaypromise <promiseukaimo@gmail.com>
2023-10-27 16:00:48 +0100

    Add users list and users order list

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -3,7 +3,7 @@ class PropertiesController < ApplicationController
 
   # GET /properties or /properties.json
   def index
-    @properties = Property.all
+    @properties = Property.order(created_at: :desc)
   end
 
   # GET /properties/1 or /properties/1.json
