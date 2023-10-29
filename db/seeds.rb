b63d2ea26a1ac59f2df673c674688331e092ca84
Ukaypromise <promiseukaimo@gmail.com>
2023-10-29 17:17:49 +0100

    Seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 # Create a default user
+User.destroy_all
 user_one = User.create(
   email: "promiseuka@gmail.com",
   first_name: "Promise",
@@ -35,7 +36,7 @@ user_three = User.create(
   role: "customer",
   confirmed_at: Time.now,
 )
-
+Propert.destroy_all
 # Create 20 sample properties with images
 10.times do |i|
   property = Property.create(
