9f32981abdeb2b802b9d18a0425d3c0626b4b545
Ukaypromise <promiseukaimo@gmail.com>
2024-02-23 21:21:01 +0100

    Add image swiper

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,12 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+})
